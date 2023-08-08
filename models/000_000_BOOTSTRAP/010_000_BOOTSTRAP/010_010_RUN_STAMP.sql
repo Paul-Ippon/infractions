5d32b33220de7e96d0cc5016097963307ae5dc11
pcolinmaire@ippon.fr <pcolinmaire@ippon.fr>
2023-08-08 13:14:00 +0000

    First commit

---
+++ models/000_000_BOOTSTRAP/010_000_BOOTSTRAP/010_010_RUN_STAMP.sql
@@ -0,0 +1,9 @@
+select 
+    '{{ category }}' as category,
+    '{{ code }}' as code,
+    '{{ extra }}' as extra,
+    '{{ invocation_id }}' as invocation_id,
+    '{{ level }}' as level,
+    '{{ log_version }}' as log_version,
+    '{{ name }}' as name,
+    '{{ ts }}' as ts
