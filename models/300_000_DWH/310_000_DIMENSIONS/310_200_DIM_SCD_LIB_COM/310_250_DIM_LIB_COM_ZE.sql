859b1830bf8c944184982718a7ebb98ca18a633b
pcolinmaire@ippon.fr <pcolinmaire@ippon.fr>
2023-08-14 15:41:48 +0000

    regex_replace

--- models/300_000_DWH/310_000_DIMENSIONS/310_200_DIM_SCD_LIB_COM/310_250_DIM_LIB_COM_ZE.sql
+++ models/300_000_DWH/310_000_DIMENSIONS/310_200_DIM_SCD_LIB_COM/310_250_DIM_LIB_COM_ZE.sql
@@ -2,12 +2,12 @@ with
     get_source as (select * from {{ ref("210_041_LIBELLES_SUPRACOMMUNAUX") }}),
     split_data as (
         select distinct
-            nivgeo,
+            regexp_replace(nivgeo, '\\d{4}', '')::varchar(7) as nivgeo,
             codgeo,
             libgeo,
             nb_com
         from get_source
-        where nivgeo = 'ZE'
+        where regexp_replace(nivgeo, '\\d{4}', '') = 'ZE'
     )
 select *
 from split_data
--- models/300_000_DWH/310_000_DIMENSIONS/310_200_DIM_SCD_LIB_COM/310_270_DIM_LIB_COM_BV.sql
+++ models/300_000_DWH/310_000_DIMENSIONS/310_200_DIM_SCD_LIB_COM/310_270_DIM_LIB_COM_BV.sql
@@ -2,12 +2,12 @@ with
     get_source as (select * from {{ ref("210_041_LIBELLES_SUPRACOMMUNAUX") }}),
     split_data as (
         select distinct
-            nivgeo,
+            regexp_replace(nivgeo, '\\d{4}', '')::varchar(7) as nivgeo,
             codgeo,
             libgeo,
             nb_com
         from get_source
-        where nivgeo = 'BV2012'
+        where regexp_replace(nivgeo, '\\d{4}', '') = 'BV'
     )
 select *
 from split_data
