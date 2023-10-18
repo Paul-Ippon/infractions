2e25ac81015b413d6f381202fbd3671dfc654cb9
mlegall@ippon.fr <mlegall@ippon.fr>
2023-10-18 07:33:26 +0000

    add yml files DSA and ODS for CITIES

---
+++ models/200_000_ODS/210_000_SCHEMA_COMPLIANCE/210_080_STG_CITIES.sql
@@ -0,0 +1,29 @@
+with
+    get_source as (
+        select *
+        from {{ source("DSA_CITIES", "CITIES") }}
+    ),
+    check_type as(
+        select
+            iff(len(inseecode) > 5, '{{ var("err_value") }}', inseecode) as inseecode,
+            iff(len(citycode) > 255, '{{ var("err_value") }}', citycode) as citycode,
+            iff(len(zipcode) > 5, '{{ var("err_value") }}', zipcode) as zipcode,
+            iff(len(label) > 255, '{{ var("err_value") }}', label) as label,
+            iff(
+                latitude is null,
+                latitude,
+                iff(try_cast(latitude as double) is not null, latitude, '{{ var("err_value") }}')
+            ) as latitude,
+            iff(
+                longitude is null,
+                longitude,
+                iff(try_cast(longitude as double) is not null, longitude, '{{ var("err_value") }}')
+            ) as longitude,
+            iff(len(departmentname) > 255, '{{ var("err_value") }}', departmentname) as departmentname,
+            iff(len(departmentnumber) > 3, '{{ var("err_value") }}', departmentnumber) as departmentnumber,
+            iff(len(regionname) > 255, '{{ var("err_value") }}', regionname) as regionname,
+            iff(len(regiongeojsonname) > 255, '{{ var("err_value") }}', regiongeojsonname) as regiongeojsonname,
+        from get_source
+    )
+select *
+from check_type
---
+++ models/200_000_ODS/210_000_SCHEMA_COMPLIANCE/210_081_CITIES.sql
@@ -0,0 +1,2 @@
+{%- set source_model = "210_080_STG_CITIES" -%}
+{%- set columns_in_relation = adapter.get_columns_in_relation(ref(source_model)) -%}
