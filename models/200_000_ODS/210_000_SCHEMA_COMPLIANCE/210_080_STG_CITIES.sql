7ddb8f5a2487b876260e8abf4967af6515a5a0b5
Paul COLINMAIRE <pcolinmaire@ippon.fr>
2023-11-28 20:33:31 +0000

    Lattitude and longitude correction

--- models/200_000_ODS/210_000_SCHEMA_COMPLIANCE/210_080_STG_CITIES.sql
+++ models/200_000_ODS/210_000_SCHEMA_COMPLIANCE/210_080_STG_CITIES.sql
@@ -9,16 +9,8 @@ with
             iff(len(citycode) > 255, '{{ var("err_value") }}', citycode) as citycode,
             iff(len(zipcode) > 5, '{{ var("err_value") }}', zipcode) as zipcode,
             iff(len(label) > 255, '{{ var("err_value") }}', label) as label,
-            iff(
-                latitude is null,
-                latitude,
-                iff(try_cast(latitude as double) is not null, latitude, '{{ var("err_value") }}')
-            ) as latitude,
-            iff(
-                longitude is null,
-                longitude,
-                iff(try_cast(longitude as double) is not null, longitude, '{{ var("err_value") }}')
-            ) as longitude,
+            iff(try_cast(latitude as double) is not null, latitude, '{{ var("err_value") }}') as latitude,
+            iff(try_cast(longitude as double) is not null, longitude, '{{ var("err_value") }}') as longitude,
             iff(len(departmentname) > 255, '{{ var("err_value") }}', departmentname) as departmentname,
             iff(len(departmentnumber) > 3, '{{ var("err_value") }}', departmentnumber) as departmentnumber,
             iff(len(regionname) > 255, '{{ var("err_value") }}', regionname) as regionname,
